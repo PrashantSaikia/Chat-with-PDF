ae3cd79c8e71be55304e09b67d04eac048c2f43b
Prashant Saikia <39755678+PrashantSaikia@users.noreply.github.com>
2023-06-19 02:10:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,101 @@
+import streamlit as st
+from dotenv import load_dotenv
+import pickle
+from PyPDF2 import PdfReader
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+import os
+ 
+# Sidebar contents
+with st.sidebar:
+
+    st.title('🤗💬 LLM Chat App')
+    st.markdown('''
+    ## About
+    This app is an LLM-powered chatbot built using:
+    - [Streamlit](https://streamlit.io/)
+    - [LangChain](https://python.langchain.com/)
+    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+ 
+    ''')
+    add_vertical_space(3)
+
+    
+    # filelist=[]
+    # for root, dirs, files in os.walk("10K_Annual_Reports/"):
+    #       for file in files:
+    #              filename=os.path.join(root, file)
+    #              filelist.append(filename)
+    # st.write('## 10-K reports present:')
+    # for file in filelist:
+    #     st.write(file.split('10K_Annual_Reports/')[1])
+
+    # add_vertical_space(1)
+
+    st.write('Made by [Prashant Saikia](https://github.com/prashantsaikia)')
+ 
+load_dotenv()
+
+os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+ 
+def main():
+    st.header("Chat with PDF 💬")
+ 
+ 
+    # upload a PDF file
+    pdf = st.file_uploader("Upload your PDF and chat with it", type='pdf')
+ 
+    # st.write(pdf)
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+ 
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+        chunks = text_splitter.split_text(text=text)
+ 
+        # # embeddings
+        store_name = pdf.name[:-4]
+        st.write(f'{store_name}')
+        # st.write(chunks)
+ 
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pkl", "rb") as f:
+                VectorStore = pickle.load(f)
+            # st.write('Embeddings Loaded from the Disk')s
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+            with open(f"{store_name}.pkl", "wb") as f:
+                pickle.dump(VectorStore, f)
+ 
+        # embeddings = OpenAIEmbeddings()
+        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+ 
+        # Accept user questions/query
+        query = st.text_input("Ask questions about your PDF file:")
+        # st.write(query)
+ 
+        if query:
+            docs = VectorStore.similarity_search(query=query, k=3)
+ 
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            st.write(response)
+ 
+if __name__ == '__main__':
+    main()
